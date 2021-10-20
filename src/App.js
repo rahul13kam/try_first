5157ac272417a434637a4b7062cee2ca8258e649
rahul13kam <91086021+rahul13kam@users.noreply.github.com>
2021-10-21 00:14:54 +0530

    my second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { ToastContainer, toast } from 'react-toastify';
+
+
 function App() {
+
+  const notify = () => toast("loremfrfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <div>
+       <div>
+       <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+/>
+        <button onClick={notify}>Notify!</button>
+        
+      </div> 
+       </div>
+    </>
+
   );
 }
 
---
+++ src/components/content.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import { Button } from 'reactstrap';
+
+
+
+export class content extends Component {
+    render() {
+        return (
+            <div>
+               
+              
+               
+            </div>
+        )
+    }
+}
+
+export default content
